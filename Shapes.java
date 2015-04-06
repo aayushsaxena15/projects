41c1c0999f590d6ce8d976d3dbd3485e95142912
aayush saxena <aayushsaxena15@users.noreply.github.com>
2015-04-06 14:54:31 +0530

    Create Shapes.java

---
+++ Shapes.java
@@ -0,0 +1,36 @@
+abstract class Shapes {
+    int a[];
+    
+    void moveTo(int new_a[]) {
+       
+    }
+    abstract void draw();
+    abstract void resize();
+}
+
+class Circle extends Shapes {
+    void draw() {
+       
+    }
+    void resize() {
+        
+    }
+}
+
+class Rectangle extends Shapes {
+    void draw() {
+        
+    }
+    void resize() {
+        
+    }
+}
+
+class Square extends Shapes {
+    void draw() {
+        
+    }
+    void resize() {
+        
+    }
+}
